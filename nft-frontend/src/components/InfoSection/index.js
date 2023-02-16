a018cd7e28ac0ed9ee102b4c6593c2fd08574c46
ARi2692 <arundhati.gunu@gmail.com>
2023-02-16 20:25:48 +0530

    Add: FolderStructure

---
+++ nft-frontend/src/components/InfoSection/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './infoSection.css'
+
+const InfoSection = () => {
+  return (
+    <div>
+      InfoSection
+    </div>
+  )
+}
+
+export default InfoSection
---
+++ nft-frontend/src/components/TrendingNFTs/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './trendingNFTs.css'
+
+const TrendingNFTs = () => {
+  return (
+    <div>
+      TrendingNFTs
+    </div>
+  )
+}
+
+export default TrendingNFTs
