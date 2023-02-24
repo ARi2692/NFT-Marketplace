7729917f65780a3bccc44fe6dbde8b34fec0a4bf
ARi2692 <arundhati.gunu@gmail.com>
2023-02-24 14:49:34 +0530

    contracts deployed and added

--- contract/scripts/deploy.js
+++ contract/scripts/deploy.js
@@ -1,23 +1,44 @@
-const { ethers, upgrades } = require("hardhat")
-
-
 async function main() {
+  const [deployer] = await ethers.getSigners();
 
-  //const candidate = await ethers.getContractFactory("CandidateContract")
-  //const proxyCandidate = await upgrades.deployProxy(job)
-  const job = await ethers.getContractFactory("JobContract")
-  const proxy = await upgrades.deployProxy(job, ["0xa4bC99F48054af823eefA73a0CC372B711099EF0"])
-  //const proxy = await upgrades.deployProxy(gateway, ["0xB5b93720E9b5F7650164b4962E8Dfe2DBdF94488", "asohmwkdib3ijn3vbbs9ejb5ja09djnwmmw"])
-  //const jobContract = await job.deploy(paramName,{value:paramvalue});
-  //await phoneLink.deployed();
-  //const proxy = await upgrades.deployProxy(proxyJob)
-  console.log("Job contract deployed to:", proxy.address)
+  console.log("Deploying contracts with the account:", deployer.address);
+  console.log("Account balance:", (await deployer.getBalance()).toString());
+
+  // Get the ContractFactories and Signers here.
+  const NFT = await ethers.getContractFactory("NFT");
+  const Marketplace = await ethers.getContractFactory("NFTMarketplace");
+  // deploy contracts
+  const marketplace = await Marketplace.deploy(1);
+  const nft = await NFT.deploy();
+  // Save copies of each contracts abi and address to the frontend.
+  saveFrontendFiles(marketplace , "NFTMarketplace");
+  saveFrontendFiles(nft , "NFT");
+}
+
+function saveFrontendFiles(contract, name) {
+  const fs = require("fs");
+  const contractsDir = __dirname + "/../../nft-frontend/src/artifacts";
+
+  if (!fs.existsSync(contractsDir)) {
+    fs.mkdirSync(contractsDir);
+  }
+
+  fs.writeFileSync(
+    contractsDir + `/${name}-address.json`,
+    JSON.stringify({ address: contract.address }, undefined, 2)
+  );
+
+  const contractArtifact = artifacts.readArtifactSync(name);
+
+  fs.writeFileSync(
+    contractsDir + `/${name}.json`,
+    JSON.stringify(contractArtifact, null, 2)
+  );
 }
 
 main()
   .then(() => process.exit(0))
-  .catch((error) => {
-    console.log("This is error")
-    console.error(error)
-    process.exit(1)
-  })
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
